d609d69479b487dae236472ddeffa383b929f6c1
Annu Priya <153050969+priyasinghanu@users.noreply.github.com>
2026-03-05 23:38:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (55.57%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def arraySign(self, nums):
+        negative_count = 0
+        
+        for num in nums:
+            if num == 0:
+                return 0
+            if num < 0:
+                negative_count += 1
+        
+        if negative_count % 2 == 0:
+            return 1
+        else:
+            return -1
